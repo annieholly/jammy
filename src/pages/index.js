5877fe423102d20044a68620ccafd97373f0c5f0
Annie Ho <girlybit@gmail.com>
2018-08-29 23:34:18 -0700

    feeling so gatsby for that whole yeear

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello friends!</h1>
+    <p>I'm Annie - nice to meet you new internet friend.</p>
+    <p>Let's build great things at freeCodeCamp JAMstack :)</p>
+  </div>
+)
+
+export default IndexPage
